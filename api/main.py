b398c26f565aa6a92e3c14847ea02bfc3465d152
Bruno Rangel <brunors1219@gmail.com>
2025-09-05 10:39:20 -0300

    test 2

--- api/main.py
+++ api/main.py
@@ -1,77 +1,58 @@
+import os
 from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pymongo import MongoClient
-from datetime import datetime
-from bson import ObjectId
+from bson.objectid import ObjectId
+
+# Caminho absoluto
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 app = FastAPI()
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 
-# Conexão MongoDB
-MONGO_URL = "mongodb+srv://bww963w_db_user:ACOnUaYEDdqKiKLQ@processos.zxlumwz.mongodb.net/?retryWrites=true&w=majority&appName=processos"
+# MongoDB Atlas
+MONGO_URL = "mongodb+srv://bww963w_db_user:<db_password>@processos.zxlumwz.mongodb.net/?retryWrites=true&w=majority&appName=processos"
 client = MongoClient(MONGO_URL)
 db = client["pwa_db"]
-processos_collection = db["processos"]
+processos_col = db["processos"]
 
+# Usuário fixo
+USUARIO_FIXO = {"usuario":"admin", "senha":"123"}
 
-USUARIO = "admin"
-SENHA = "123"
-
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-# -------------------- ROTAS --------------------
-
-@app.get("/", response_class=HTMLResponse)
-async def login_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request, "erro": None})
+# ----- LOGIN -----
+@app.get("/")
+def login_get(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request, "hide_menu": True})
 
 @app.post("/login")
-async def login(request: Request, usuario: str = Form(...), senha: str = Form(...)):
-    if usuario == USUARIO and senha == SENHA:
-        return RedirectResponse(url="/cadastro", status_code=303)
-    return templates.TemplateResponse("login.html", {"request": request, "erro": "Usuário ou senha inválidos"})
+def login_post(request: Request, usuario: str = Form(...), senha: str = Form(...)):
+    if usuario == USUARIO_FIXO["usuario"] and senha == USUARIO_FIXO["senha"]:
+        response = RedirectResponse("/cadastro", status_code=303)
+        return response
+    return templates.TemplateResponse("login.html", {"request": request, "erro": "Usuário ou senha incorretos", "hide_menu": True})
 
-@app.get("/cadastro", response_class=HTMLResponse)
-async def cadastro(request: Request):
+# ----- CADASTRO PROCESSO -----
+@app.get("/cadastro")
+def cadastro_get(request: Request):
     return templates.TemplateResponse("cadastro_processo.html", {"request": request})
 
 @app.post("/cadastro")
-async def cadastro_process(numero: str = Form(...), descricao: str = Form(...)):
-    processo = {
-        "numero": numero,
-        "ultima_atualizacao": datetime.now(),
-        "detalhes": [{"descricao": descricao, "data": datetime.now()}]
-    }
-    processos_collection.insert_one(processo)
-    return RedirectResponse(url="/processos", status_code=303)
+def cadastro_post(request: Request, numero: str = Form(...), descricao: str = Form(...)):
+    processo = {"numero": numero, "descricao": descricao}
+    processos_col.insert_one(processo)
+    return RedirectResponse("/processos", status_code=303)
 
-@app.get("/processos", response_class=HTMLResponse)
-async def listar_processos(request: Request):
-    processos = list(processos_collection.find())
-    for p in processos:
-        p["_id"] = str(p["_id"])
+# ----- LISTA PROCESSOS -----
+@app.get("/processos")
+def processos_get(request: Request):
+    processos = list(processos_col.find({}))
     return templates.TemplateResponse("processos_processo.html", {"request": request, "processos": processos})
 
-@app.get("/processo/{processo_id}", response_class=HTMLResponse)
-async def detalhes_processo(request: Request, processo_id: str):
-    processo = processos_collection.find_one({"_id": ObjectId(processo_id)})
-    if processo:
-        processo["_id"] = str(processo["_id"])
-        processo["detalhes"] = sorted(processo["detalhes"], key=lambda d: d["data"], reverse=True)
+# ----- DETALHES PROCESSO -----
+@app.get("/detalhes/{processo_id}")
+def detalhes_get(request: Request, processo_id: str):
+    processo = processos_col.find_one({"_id": ObjectId(processo_id)})
     return templates.TemplateResponse("detalhes_processo.html", {"request": request, "processo": processo})
-
-@app.post("/processo/{processo_id}/add")
-async def add_detalhe(processo_id: str, descricao: str = Form(...)):
-    processos_collection.update_one(
-        {"_id": ObjectId(processo_id)},
-        {"$push": {"detalhes": {"descricao": descricao, "data": datetime.now()}},
-         "$set": {"ultima_atualizacao": datetime.now()}}
-    )
-    return RedirectResponse(url=f"/processo/{processo_id}", status_code=303)
-
-@app.get("/logout")
-async def logout():
-    return RedirectResponse(url="/")
